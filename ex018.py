59aad9e70e1fc596bec809579a25974bd32866cf
Gabriel Liszt Ferreira de Oliveira <141736171+Gabriel-Liszt@users.noreply.github.com>
2024-02-05 19:29:38 -0300

    att

--- ex018.py
+++ ex018.py
@@ -6,5 +6,5 @@ seno = math.sin(rad)
 coseno = math.cos(rad)
 tangente = math.tan(rad)
 
-print(f'O seno, coseno e tangente de {ang} são respectivamente {seno}, {coseno} e {tangente}')
+print(f'O seno, coseno e tangente de {ang}º são respectivamente {seno}, {coseno} e {tangente}')
 
